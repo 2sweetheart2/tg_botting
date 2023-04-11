c16e64ca6956de65020d34892a164e7e5785d78a
2sweetheart2 <2004sweetheart2004@gmail.com>
2023-04-11 21:03:45 +0300

    1.5.9

--- tg_botting/objects.py
+++ tg_botting/objects.py
@@ -270,14 +270,17 @@ class Message:
             data['parse_mode'] = parse_mode
         if reply_markup:
             data['reply_markup'] = json.dumps(reply_markup.to_dict())
-        if len(text) > 4096:
-            data['text'] = text[0:4096]
-            rs = await self.bot.tg_request('sendMessage', True, **data)
         if photo:
             data['caption'] = text
             data.pop('chat_id')
             return await self.send_photo(photo, **data)
-        data['text'] = text[4096:len(text)]
+        if len(text) > 4096:
+            data['text'] = text[0:4096]
+            rs = await self.bot.tg_request('sendMessage', True, **data)
+            data['text'] = text[4096:len(text)]
+            rs = await self.bot.tg_request('sendMessage', True, **data)
+            return rs.get('ok')
+        data['text'] = text
         rs = await self.bot.tg_request('sendMessage', True, **data)
         return rs.get('ok')
 
